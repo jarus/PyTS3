5c029d4cfa5a0f8959002bf9d1c1146f122a61d4
Christoph Heer <Christoph.Heer@googlemail.com>
2010-07-16 22:40:17 +0200

    * Mehr Informationen in das Setup Skript eingefügt

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
 setup( 
-     name = "PyTS3", 
-     version = "0.2", 
-     author = "Christoph Heer", 
-     author_email = "Christoph.Heer@googlemail.com", 
-     py_modules = ["PyTS3"] 
+    name = "PyTS3", 
+    version = "$Rev$",
+ 	description = "PyTS3 is a little modul for Python, that you can better work with your TS3-Query in your Python projects."
+    author = "Christoph Heer", 
+    author_email = "Christoph.Heer@googlemail.com", 
+    license="MIT License",
+	url="http://redmine.thelabmill.de",
+	py_modules = ["PyTS3"] 
      )
